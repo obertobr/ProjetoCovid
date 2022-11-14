6a2038bc22b4b1832049ce7026abc216cc50d312
obertobr <real.azzi@gmail.com>
2022-11-14 15:28:10 -0300

    Uso da api removido da pagina principal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './App.css';
-import axios from 'axios';
 import icon from '../src/icons/icon.svg'
 import home from '../src/icons/home.svg'
 import graph from '../src/icons/graph.svg'
 import info from '../src/icons/info.svg'
 
-
-const token = "b8075c2278f4fbd8c647965245d83431fcafc758"
-
 function App() {
-  useEffect(() =>{
-    axios.get(`https://api.brasil.io/v1/dataset/covid19/caso/data/?state=SP&is_last=True&place_type=state`, {
-      headers: {
-        Authorization: 'Token ' + token
-      }
-    })
-      .then(res => {
-        console.log(res.data);
-      })
-  }, [])
-
   return (
     <>
       <div id="NavBar">
