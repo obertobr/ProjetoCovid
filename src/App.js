d7a019e4df943d6b22f58384cdb01cd88ead5b72
obertobr <real.azzi@gmail.com>
2022-11-14 15:23:01 -0300

    NavBar adcionada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react'
 import './App.css';
 import axios from 'axios';
+import icon from '../src/icons/icon.svg'
+import home from '../src/icons/home.svg'
+import graph from '../src/icons/graph.svg'
+import info from '../src/icons/info.svg'
+
 
 const token = "b8075c2278f4fbd8c647965245d83431fcafc758"
 
@@ -17,9 +22,14 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div id="NavBar">
+        <img id="icon" src={icon} alt="Lupa"/>
+        <img src={home} alt="Home"/>
+        <img src={graph} alt="Graficos"/>
+        <img src={info} alt="Sobre"/>
+      </div>
+    </>
   );
 }
 
