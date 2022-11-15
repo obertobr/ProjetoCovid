8a9ceb3d964958231e5d7ff5690c6d9da3c1a86a
LucksBr <93302128+LucksBr@users.noreply.github.com>
2022-11-15 02:10:42 -0300

    Adicionado parte de analise

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React from 'react'
-import './App.css';
+import './styles/Header.css';
+import './styles/Global.css'
 import icon from '../src/icons/icon.svg'
 import home from '../src/icons/home.svg'
 import graph from '../src/icons/graph.svg'
 import info from '../src/icons/info.svg'
+import { useState } from 'react';
+
+import { Home } from './components/Home';
 
 function App() {
+
+  let [homeOn,setHomeOn] = useState(true)
+  let [analiseOn, setAnaliseOn] = useState(false)
+  let [sobreOn, setSobreOn] = useState(false)
+  
+
+  let alterarConteudoMain = (area) => {
+
+    if (area === "home") {
+
+      setHomeOn(true)
+      setAnaliseOn(false)
+      setSobreOn(false)
+
+    } else if (area === "analise") {
+
+      setHomeOn(false)
+      setAnaliseOn(true)
+      setSobreOn(false)
+
+    } else {
+
+      setHomeOn(false)
+      setAnaliseOn(false)
+      setSobreOn(true)
+
+    }
+
+  }
+
   return (
     <>
       <div id="NavBar">
-        <img id="icon" src={icon} alt="Lupa"/>
-        <img src={home} alt="Home"/>
-        <img src={graph} alt="Graficos"/>
-        <img src={info} alt="Sobre"/>
+        <img id="icon" src={icon} alt="Lupa" />
+        <img className='logo' onClick={() => alterarConteudoMain("home")} src={home} alt="Home"/>
+        <img className='logo' onClick={() => alterarConteudoMain("analise")} src={graph} alt="Graficos"/>
+        <img className='logo' onClick={() => alterarConteudoMain("sobre")} src={info} alt="Sobre"/>
       </div>
+
+      <main>
+        {
+          homeOn ? (
+          <>
+            <Home></Home>
+           </>
+          ) : analiseOn ? (<h2>Analise</h2>) : 
+          sobreOn ? (<h2>Sobre</h2>) : ""
+        }
+      </main>
     </>
   );
 }
