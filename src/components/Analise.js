41773d9b68e29eb9c5a0b8e043c45cc748898f40
LucksBr <93302128+LucksBr@users.noreply.github.com>
2022-11-17 00:07:30 -0300

    a
    a

--- src/components/Analise.js
+++ src/components/Analise.js
@@ -14,7 +14,7 @@ import {
   import { Bar,Pie } from 'react-chartjs-2';
   
 
-  const estados = ["Brasil","AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"]
+  const estados = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"]
 
   ChartJS.register(
     CategoryScale,
@@ -45,15 +45,37 @@ import {
 
 export function Analise (props) {
 
+  let formatarNumero = (numero) => {
+
+            
+    let tamanho = numero.length 
+    let resto = tamanho % 3 
+    let separacoes = (tamanho - resto) / 3 
+    let numeroFormatado = ""
+
+    numeroFormatado += numero.substring(0, resto)
+
+    for (let index = 0; index < separacoes; index++) {
+        
+       let aux =  index * 3 + resto
+
+       if (resto === 0 && index === 0 ) {
+        numeroFormatado += numero.substring(aux,aux + 3)
+       } else {
+        numeroFormatado += "." + numero.substring(aux,aux + 3)
+       }
+
+    }
+   
+    return numeroFormatado
+
+}
+
     let [data,setData] = useState(
         {
             labels,
             datasets: [
-              {
-                label: 'População',
-                data: [0,0,0,0,0,0,0,0,0,0,0,0],
-                backgroundColor: '#A732DE',
-              },
+              
               {
                 label: 'Casos Confirmados',
                 data: [0,0,0,0,0,0,0,0,0,0,0,0],
@@ -89,73 +111,111 @@ export function Analise (props) {
         ],
       })
    
-    let [dadosGraficoAnual,setDadosGraficoAnual] = useState(["Brasil","2021"])
+    let [dadosGraficoAnual,setDadosGraficoAnual] = useState(["SP","2021"])
     
    let [mes,setMes] = useState("Geral")
 
     let atualizarGrafico = async() => {
 
         let dados = {
-            populacao : [],
-            casos : [],
-            obitos : [],
+            casos : [0,0,0,0,0,0,0,0,0,0,0,0],
+            obitos : [10,0,0,0,0,0,0,0,0,0,0,0],
         }
         
         for (let index = 0; index < 12; index++) {
-            dados.populacao[index] = Math.round(Math.random() * 100 )
-            dados.casos[index] = Math.round(Math.random() * 80 + 20)
-            dados.obitos[index] = Math.round(Math.random() * 40 + 10)
-        }
 
-        setData(
-            {
-                labels,
-                datasets: [
-                  {
-                    label: 'População',
-                    data: dados.populacao,
-                    backgroundColor: '#A732DE',
-                  },
-                  {
-                    label: 'Casos Confirmados',
-                    data: dados.casos,
-                    backgroundColor: '#FDCC56',
-                  },
-                  {
-                    label: 'Óbitos',
-                    data: dados.obitos,
-                    backgroundColor: '#FD5E58',
-                  },
-                ],
-              }
-        )
-       
-       
+          let mes = (index + 1) >= 10 ? (index + 1) : "0" + (index + 1)
+   
+
+          try {
+
+            var dado = await props.pegarDadosPorEstado(dadosGraficoAnual[0],dadosGraficoAnual[1] + "-" + mes)
+          } catch(e){
+            dados.data = 0
+            console.log(e)
+          }
+
+          console.log(dado)
+
+          if(dado) {
+            dados.casos[index] = dado.data[4]
+            dados.obitos[index] = dado.data[5]
+          } else {
+            dados.casos[index] = 0
+            dados.obitos[index] = 0
+          }
+
+            setData(
+              {
+                  labels,
+                  datasets: [
+                    
+                    {
+                      label: 'Casos Confirmados',
+                      data: dados.casos,
+                      backgroundColor: '#FDCC56',
+                    },
+                    {
+                      label: 'Óbitos',
+                      data: dados.obitos,
+                      backgroundColor: '#FD5E58',
+                    },
+                  ],
+                }
+          )
+        }
 
     }
 
     useEffect(() => {
         atualizarGrafico()
 
-    },[dadosGraficoAnual])
+    },[ dadosGraficoAnual])
 
     let [graficoPizza,setGraficoPizza] = useState(false)
 
-    let atualizarGraficoPizza = () => {
+    let atualizarGraficoPizza = async () => {
 
-        let dados = {
-            populacao : Math.round(Math.random() * 100 + 40 ),
-            casos : Math.round(Math.random() * 100 + 20 ),
-            obitos : Math.round(Math.random() * 100 ),
-        }
+      try {
+        let Mes = mes < 10 ? "0"+mes : mes
+        var dado = await props.pegarDadosPorEstado(dadosGraficoAnual[0],dadosGraficoAnual[1] + "-" + Mes)
+      } catch(e){
+        dado= 0
+        console.log(e)
+      }
+    
+      let dados
+
+      if(dado) {
+        dados = {
+          populacao : dado.data[9]? dado.data[9] : 0,
+          casos : dado.data[4]? dado.data[4] : 0,
+          obitos : dado.data[5]? dado.data[5] : 0,
+      }
+      
+    } else {
+
+        dados = {
+          populacao : 0,
+          casos :  0,
+          obitos :  0,
+      }
+      
+
+      }
+       
 
         if(mes !== "Geral") {
             setGraficoPizza(true)
+           
             setDataPizza({
                 labels: ['População', 'Casos Confirmados', 'Óbitos'],
                 datasets: [
                   {
-                    data: [dados.populacao, dados.casos, dados.obitos],
+                    data: [dados.populacao,
+                      dados.casos, 
+                      dados.obitos
+                       ],
                     backgroundColor: [
                       '#5A6A8A',
                       '#8AA2D2',
@@ -182,7 +242,7 @@ export function Analise (props) {
 
     useEffect(() => {
         atualizarGraficoPizza()
-    },[mes,dadosGraficoAnual])
+    },[mes])
 
     return (
         <>
@@ -211,7 +271,7 @@ export function Analise (props) {
                         </div>
                         
                         <div id="dados">
-                            <h3>População: {dataPizza.datasets[0].data[0]}</h3>
+                            <h3>População: {formatarNumero(dataPizza.datasets[0].data[0] + "")}</h3>
                             <h3>Taxa de Casos: {(dataPizza.datasets[0].data[1]/dataPizza.datasets[0].data[0] * 100).toFixed(3) + "%"}</h3>
                             <h3>Taxa de Óbitos: {(dataPizza.datasets[0].data[2]/dataPizza.datasets[0].data[1] * 100).toFixed(3) + "%"}</h3>
                         </div>
@@ -244,6 +304,7 @@ export function Analise (props) {
                             }}>
                                 <option value="2021">2021</option>
                                 <option value="2020">2020</option>
+                                
                             </select>
                         </div>
 
@@ -251,9 +312,10 @@ export function Analise (props) {
                             <h3>Mês</h3>
                             <select value={mes} onChange={(e) => {
                                 setMes(e.target.value)
+
                             }}>
                                 {opcoesData.map((mes,i) => {
-                                    return <option key={i} value={mes}>{mes}</option>
+                                    return <option key={i} value={i+1}>{mes}</option>
                                 })}
                             </select>
                         </div>
